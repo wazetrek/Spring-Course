4e41f778d3cb0c61206cb4ce6c29629ae02b6250
Andrés Blandón <blandonfelipe1@gmail.com>
2020-04-10 20:32:32 -0500

    Lección 5
    Se agrega el caso de editar y eliminar al proyecto

--- src/main/java/com/course/HelloSpring2/web/PersonaController.java
+++ src/main/java/com/course/HelloSpring2/web/PersonaController.java
@@ -1,10 +1,12 @@
 package com.course.HelloSpring2.web;
 
+import com.course.HelloSpring2.domain.Persona;
 import com.course.HelloSpring2.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -18,5 +20,29 @@ public class PersonaController {
         var personas = personaService.listarPersonas();
         model.addAttribute("personas", personas);
         return "indexPersonas";
+    }
+
+    @GetMapping("/agregar")
+    public String agregar(Persona persona) {
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona) {
+        personaService.guardar(persona);
+        return "redirect:/personas";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editar(Persona persona, Model model) {
+        persona = personaService.encontrar(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona) {
+        personaService.eliminar(persona);
+        return "redirect:/personas";
     }
 }
